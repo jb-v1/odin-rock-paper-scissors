8d4cbb264b1735346a70ecbfa4d705a9fde27d7c
jbv <brianvilbar@gmail.com>
2022-09-22 10:51:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random()*3);
     let choice;
-    switch(randomNum){
+    switch (randomNum) {
         case 0:
             choice = "rock";
             break;
@@ -18,20 +18,55 @@ function getComputerChoice() {
 
 // get computer choice
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
+console.log("computer chose: " + computerSelection);
 
 // get VALID user choice. If invalid, repeat prompt
 let rawInput;
-while(rawInput != "rock" && rawInput != "paper" && rawInput != "scissors"){
+while (rawInput != "rock" && rawInput != "paper" && rawInput != "scissors") {
     let askUser = prompt("Rock, paper or scissors?");
     rawInput = askUser.toLowerCase();
-    console.log(rawInput);
 }
 const playerSelection = rawInput;
-console.log(playerSelection);
 
-// a round of RPS. Get both user and computer choice.
-	// if equal, tie.
-	// rock vs scissors match up. Rock wins.  
-	// scissors vs paper match up. Scissors wins.
-	// paper vs rock match up. Paper wins
+// Track score
+let playerScore = 0;
+let computerScore = 0;
+
+// Play One Round
+function playRound(player, computer){
+    let roundMessage;
+    switch (true) {
+        // if equal, tie.
+        case (player == computer):
+            console.log("Tie.");
+            break;
+        // rock vs scissors match. Rock wins.  
+        case (player == "rock" || player == "scissors") 
+        && (computer == "rock" || computer == "scissors"):
+            roundMessage = "Rock beats scissors.";
+            if (player == "rock") {playerScore++} else {computerScore++};
+            break;
+        // scissors vs paper match. Scissors wins.
+        case (player == "scissors" || player == "paper") 
+        && (computer == "scissors" || computer == "paper"):
+            roundMessage = "Scissors beats paper.";
+            if (player == "scissors") {playerScore++} else {computerScore++};
+            break;
+        // paper vs rock match. Paper wins.
+        case (player == "paper" || player == "rock") 
+        && (computer == "paper" || computer == "rock"):
+            roundMessage = "Paper beats rock.";
+            if (player == "paper") {playerScore++} else {computerScore++};
+            break;
+    };
+    if (playerScore > computerScore){
+        console.log("You Win! " + roundMessage);
+        return playerScore;
+    }else if (computerScore > playerScore) {
+        console.log("You Lose! " + roundMessage);
+        return computerScore;
+    }
+};
+
+// run
+playRound(playerSelection, computerSelection);
